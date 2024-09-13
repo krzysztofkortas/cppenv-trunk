7900b31647b98e842fd3976556cf5eb7870cde9e
Krzysztof Kortas <krzysztofkortas96@gmail.com>
2024-09-13 23:56:43 +0200

    Remove unused files

--- build-amd64.sh
+++
@@ -1,2 +0,0 @@
-TAG=20240118
-docker buildx build --no-cache --push --platform linux/amd64 --tag krzysztofkortas/devenv-trunk:latest-amd64 --tag krzysztofkortas/devenv-trunk:${TAG}-amd64 .
--- retag.bat
+++
@@ -1,9 +0,0 @@
-set TAG=20240118
-
-docker manifest create krzysztofkortas/devenv-trunk:latest --amend krzysztofkortas/devenv-trunk:latest-amd64
-docker manifest annotate krzysztofkortas/devenv-trunk:latest krzysztofkortas/devenv-trunk:latest-amd64 --arch amd64
-docker manifest push krzysztofkortas/devenv-trunk:latest
-
-docker manifest create krzysztofkortas/devenv-trunk:%TAG% --amend krzysztofkortas/devenv-trunk:%TAG%-amd64
-docker manifest annotate krzysztofkortas/devenv-trunk:%TAG% krzysztofkortas/devenv-trunk:%TAG%-amd64 --arch amd64
-docker manifest push krzysztofkortas/devenv-trunk:%TAG%
